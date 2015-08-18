bbbb551c1650d789a5a44b2af50643f840a29183
Varun Patel <info@corewaysolution.com>
2015-08-18 13:43:45 +0530

    Update index.php

--- api/src/File_k/index.php
+++ api/src/File_k/index.php
@@ -1,4 +1,4 @@
 <?php
-define("CLI_ID","configure git to ignore changes in tracked file");
-define("CLI_SEC","configure git to ignore changes in tracked file");
+define("CLI_ID","configure");
+define("CLI_SEC","configure");
 ?>
